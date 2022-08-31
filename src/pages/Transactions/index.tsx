4185e4b4ad55f941f28d232b95ff0ba929746f99
rafinhaa <rafinhaa@me.com>
2022-08-31 10:41:25 -0300

    Estrutura visual - Componente: SearchForm

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./sections/SearchForm";
 import {
   PriceHighlight,
   TransactionsContainer,
@@ -12,6 +13,7 @@ export const Transactions = () => {
       <Header />
       <Summary />
       <TransactionsContainer>
+        <SearchForm />
         <TransactionsTable>
           <tbody>
             <tr>
