d071dd58252098fa94686cfec3fee51ceca7547a
rafinhaa <rafinhaa@me.com>
2022-08-31 10:11:30 -0300

    Estrutura visual - Tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,34 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export const Transactions = () => {
   return (
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <PriceHighlight variant="outcome">-R$ 12.000,00</PriceHighlight>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 };
